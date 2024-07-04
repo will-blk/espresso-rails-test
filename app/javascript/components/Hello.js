3638c960edf5e922dd38d66101445ce0c49e5cf8
Walter Silva <walter@espressoapp.com.br>
2024-07-04 08:09:44 -0300

    Criação do componente react exemplo

--- app/javascript/components/Hello.js
+++ app/javascript/components/Hello.js
@@ -1,16 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
 
 const Hello = (props) => {
   return (
     <React.Fragment>
-      Greeting: {props.greeting}
+      <Container maxWidth="sm">
+        <Typography variant="h2" component="h1" align="center">
+          Olá, {props.candidate}!
+        </Typography>
+
+        <Typography variant="subtitle1" component="p" align="center">
+          Seu teste técnico começa aqui.
+        </Typography>
+      </Container>
     </React.Fragment>
   )
 }
 
 Hello.propTypes = {
-  greeting: PropTypes.string
+  candidate: PropTypes.string
 };
 
 export default Hello
