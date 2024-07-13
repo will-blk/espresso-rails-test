80d27842fef460822d9d9178ed169c0d603e9aa7
Will <will-blk@hotmail.com>
2024-07-13 11:51:06 -0300

    Add pundit and settings

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'react-rails', '~> 3.2', '>= 3.2.1'
 
 gem 'devise'
 
+gem 'pundit'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  after_action :verify_authorized
+
   rescue_from ActionController::ParameterMissing, with: -> { head :bad_request }
 end
