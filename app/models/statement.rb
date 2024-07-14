10bf1dc8a53a5038bf6d57ddb609d50dc731fbb0
Will <will-blk@hotmail.com>
2024-07-14 11:35:40 -0300

    Add invoice attachment to statement

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -3,7 +3,9 @@
 class Statement < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :card
+
   has_one :company, through: :card
+  has_one_attached :invoice
 
   validates :cost, :merchant, :performed_at, :transaction_id, presence: true
 
